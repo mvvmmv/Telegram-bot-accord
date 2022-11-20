3e1d26a36ac088004449275ab3d8fb7a1b7defaf
mvvmmv <iva104@gmail.com>
2022-11-20 20:16:47 +0300

    Added main file and functions module

---
+++ bot.py
@@ -0,0 +1,132 @@
+import telebot
+from datetime import datetime
+import logging
+
+import functions
+
+BOT_API = ''
+bot = telebot.TeleBot(BOT_API)
+input_key = ''
+input_lad = ''
+
+logging.basicConfig(
+    level=logging.ERROR,
+    filename="errorlog.log",
+    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: \
+              %(lineno)d - %(message)s",
+    datefmt='%H:%M:%S',
+)
+
+
+def logging_decorator(func):
+    '''Декоратор логирования'''
+    def wrapper(message):
+        with open('log.txt', 'a') as f:
+            f.write(str(message.text)+'  ' +
+                    str(datetime.fromtimestamp(message.date))+'  ' +
+                    str(message.from_user.id)+'  ' +
+                    str(message.from_user.is_bot)+'  ' +
+                    str(message.from_user.first_name)+'  ' +
+                    str(message.from_user.last_name)+'  ' +
+                    str(message.from_user.username)+'  ' +
+                    str(message.from_user.language_code)+'\n')
+        func(message)
+    return wrapper
+
+
+@bot.message_handler(commands=['start'])
+@logging_decorator
+def start(message):
+    '''Обработчик команды /start'''
+
+    bot.send_message(message.chat.id, 'Привет, '+str(message.chat.first_name) +
+                     '. Чтобы узнать что может бот введи команду /help')
+
+
+@bot.message_handler(commands=['help'])
+@logging_decorator
+def help(message):
+    '''Обработчик команды /help'''
+
+    bot.send_message(message.chat.id,
+                     "Список возможных запросов:\n<b>Лады</b>, <b>лад</b> - покажет все лады, имеющиеся в программе\n<b>Тональность</b>, <b>тон</b>, <b>ноты</b>, <b>нота</b> - покажет все ноты\n<b>Гамма</b> - покажет гамму в заданном ладу от заданной ноты\n<b>Аккорды</b>, <b>аккорд</b> - покажет основные аккорды в заданной тональности", parse_mode='html')
+
+
+@bot.message_handler(content_types=['text'])
+@logging_decorator
+def get_user_text(message):
+    '''Обработчик текста, вводимого пользователем'''
+
+    input_text = (message.text).lower()
+    if input_text in ['лады', 'лад']:
+        bot.send_message(message.chat.id, ', '.join(list(functions.get_harmonies().keys())))
+    elif input_text in ['тональность', 'тон', 'ноты', 'нота']:
+        bot.send_message(message.chat.id, '  '.join(functions.get_gamma()))
+
+    elif input_text in ['гамма']:
+        # спросить тон, лад (по умолчанию натуральный мажор)
+        # рисуется клавиатура со всеми ключами из get_gamma
+        key = draw_buttons(functions.get_gamma())
+        msg = bot.send_message(
+            message.chat.id, 'В каком ключе?', reply_markup=key)
+        bot.register_next_step_handler(msg, get_key, message.text)
+
+    elif input_text in ['аккорды', 'аккорд']:
+        # спросить тон, лад (по умолчанию натуральный мажор)
+        key = draw_buttons(functions.get_gamma())
+        msg = bot.send_message(
+            message.chat.id, 'В каком ключе?', reply_markup=key)
+        bot.register_next_step_handler(msg, get_key, input_text)
+    else:
+        msg = bot.send_message(
+            message.chat.id, 'Не понял. Попробуйте другой запрос.\nНапример, "гамма". "лад", "тональность", "аккорды"')
+
+
+def get_key(message, init_request):
+    '''Сохраняет ответ пользователя(нота) и задает следующий вопрос(какой
+    лад)'''
+
+    global input_key
+    input_key = message.text
+
+    if input_key not in functions.get_gamma():
+        bot.send_message(message.chat.id, 'Введенное значение некорректно')
+    else:
+        harmony = draw_buttons(list(functions.get_harmonies().keys()))
+        msg = bot.send_message(
+            message.chat.id, 'Какой лад?', reply_markup=harmony)
+        bot.register_next_step_handler(msg, get_harm, init_request)
+
+
+def get_harm(message, init_request):
+    '''Сохраняет ответ пользователя(лад) и отправляет в ответ
+    аккорды или гамму в зависимости от запроса init_request и предыдущего
+    ответа'''
+
+    global input_lad
+    input_lad = message.text
+
+    if input_lad not in list(functions.get_harmonies().keys()):
+        bot.send_message(message.chat.id, 'Введенное значение некорректно')
+    else:
+        if init_request in ['гамма']:
+            gamma = functions.get_gamma_by_key(input_key, input_lad)
+            bot.send_message(message.chat.id, '  '.join(gamma))
+        elif init_request in ['аккорды', 'аккорд']:
+            chords = functions.get_chords_by_key_and_harmony(
+                input_key, input_lad)
+            bot.send_message(message.chat.id, '  '.join(chords))
+
+
+def draw_buttons(list: list):
+    '''Рисует клавиатуру для ответов пользователя'''
+
+    markup = telebot.types.ReplyKeyboardMarkup(
+        one_time_keyboard=True, resize_keyboard=True, row_width=3)
+    for text in list:
+        button = telebot.types.KeyboardButton(text)
+        markup.add(button)
+    return markup
+
+
+bot.polling(non_stop=True)
---
+++ functions.py
@@ -0,0 +1,148 @@
+import re
+
+
+def get_gammas_by_harmony(lad='натуральный мажор'):
+    '''lad - название лада,
+    возвращаемое значение - список гамм от всех нот,
+    соответствующий заданному lad'''
+
+    # все ноты хроматической гаммы
+    gamma = get_gamma()
+    # все лады
+    harmonies = get_harmonies()
+
+    # проверка на корректность лада
+    try:
+        lad = harmonies[lad]
+    except Exception:
+        return ['Ошибка: Некорректный лад']
+
+    gammas = []
+
+    # строим гаммы для лада начиная с каждой ноты лада
+    # гаммы представлены в виде 0 и 1
+    for i in range(len(lad)):
+        new_gamma = gamma[i:] + gamma[:i]
+        gammas.append(new_gamma)
+
+    nn_gammas = []
+
+    # переводим каждую гамму в буквенную
+    for gamma in gammas:
+        nn_gammas.append([gamma[j] for j in range(len(lad)) if lad[j] == 1])
+
+    return nn_gammas
+
+
+def get_gamma_by_key(key, lad='натуральный мажор'):
+    '''Получить гамму по заданному ключу (тону) и ладу.
+    key - ключ (тон), lad - название лада'''
+
+    # проверка на корректность ключа
+    gamma_copy = get_gamma().copy()
+    gamma_copy.extend(get_sharp_flats().values())
+    if key not in gamma_copy:
+        return ['Ошибка: Некорректный ключ']
+
+    # получить нужную гамму из всех построенных
+    for gamma in GAMMAS[lad]:
+        if gamma[0] == key:
+            return (sharp_to_flat_check(gamma))
+        #    return (gamma)
+
+
+def get_all_gammas():
+    '''Получить все гаммы для всех ключей и ладов'''
+
+    all_gammas = {}
+    for lad in get_harmonies().keys():
+        all_gammas[lad] = get_gammas_by_harmony(lad)
+    return all_gammas
+
+
+def sharp_to_flat_check(gamma):
+    '''Проверить есть ли в гамме ноты с диезами, которые надо заменить
+    на соответствующие ноты с бемолями'''
+
+    new_gamma = gamma.copy()
+    # если в гамме есть две одинаковые ноты (одна с диезом, одна - без)
+    # то, заменяем их в соответствии с sharp_flat
+    if re.search(r'(\w)\1', ''.join(gamma)):
+        new_gamma = replace_sharps(gamma)
+    # если в гамме все ноты с диезами
+    # то, заменяем их в соответствии с sharp_flat
+    elif ''.join(gamma).count('#') == 7:
+        new_gamma = replace_sharps(gamma)
+    return new_gamma
+
+
+def get_sharp_flats():
+    sharp_flat = {
+        'C#': 'Db',
+        'D#': 'Eb',
+        'E#': 'F',
+        'F#': 'Gb',
+        'G#': 'Ab',
+        'A#': 'Hb',
+        'H#': 'C'
+    }
+    return sharp_flat
+
+
+def replace_sharps(gamma):
+    '''Заменить ноты с диезами на соотв. ноты с бемолями'''
+
+    sharp_flat = get_sharp_flats()
+    new_gamma = []
+    for key in gamma:
+        if '#' in key:
+            new_gamma.append(sharp_flat[key])
+        else:
+            new_gamma.append(key)
+    return new_gamma
+
+
+def get_chords_by_key_and_harmony(key: str, lad='натуральный мажор'):
+    '''key - буквенное обозначение ноты,
+    lad - название лада,
+    возвращаемое значение - список аккордов в заданной тональности'''
+
+    # получаем нужную гамму
+    gamma = get_gamma_by_key(key, lad)
+
+    # строим аккорды
+    T53 = gamma[0]+gamma[2]+gamma[4]
+    T6 = gamma[2]+gamma[4]+gamma[0]
+    T64 = gamma[4]+gamma[0]+gamma[2]
+
+    S53 = gamma[3]+gamma[5]+gamma[0]
+    S6 = gamma[5]+gamma[0]+gamma[3]
+    S64 = gamma[0]+gamma[5]+gamma[3]
+
+    D53 = gamma[4]+gamma[6]+gamma[1]
+    D6 = gamma[6]+gamma[1]+gamma[4]
+    D64 = gamma[1]+gamma[4]+gamma[6]
+
+    return ['T ', T53, T6, T64, '\nS ', S53, S6, S64, '\nD ', D53, D6, D64]
+
+
+def get_gamma():
+    '''Получить хроматическую гамму от ноты До до ноты СИ'''
+    return ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'H']
+
+
+def get_harmonies():
+
+    # 12 хроматических нот (1 - нота в гамме, 0 - нота отсутствует в гамме)
+    harmonies = {
+        'натуральный мажор':      [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+        'натуральный минор':      [1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1],
+        'гармонический мажор':    [1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1],
+        'гармонический минор':    [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1],
+        'локрийский лад (минор)': [1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
+        'фригийский лад (минор)': [1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]
+    }
+    return harmonies
+
+
+GAMMAS = get_all_gammas()
